4910b399e923d807c0281b1105ba1915cdabcb02
Jackie Lin <jackielin2025@u.northwestern.edu>
2023-07-24 22:32:59 -0400

    refactor index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { PortalProvider } from '@gorhom/portal';
@@ -7,44 +7,40 @@ import { StatusBar } from 'expo-status-bar';
 import Home from './views/Home/Home';
 import ShoppingCartOrderScreen from './views/ShoppingCartOrderScreen/ShoppingCartOrderScreen';
 import { Audio } from 'expo-av';
+import AddContent from './views/AddContent/AddContent';
 
 // https://reactnavigation.org/docs/material-bottom-tab-navigator
 const Tab = createMaterialBottomTabNavigator();
 const Stack = createStackNavigator();
 
-const createTabs = () => {
-  const tabNames = ['home', 'cart', 'create', 'inbox', 'profile'];
-  const tabComponents = [Home, ShoppingCartOrderScreen, Home, Home, Home];
-
-  const tabs = [];
-  for (let i = 0; i < tabNames.length; i++) {
-    tabs.push(
-      <Tab.Screen
-        key={tabNames[i]}
-        name={tabNames[i]}
-        component={tabComponents[i]}
-        options={{
-          tabBarIcon: () => (
-            <MaterialCommunityIcons name={tabNames[0]} size={26} />
-          ), // replace later
-        }}
-      />
-    );
-  }
-
-  return tabs;
-};
-
 const TabNavigator = () => {
+  const [showTabs, setShowTabs] = useState(true);
+
+  // const tabNames = ['home', 'cart', 'create', 'inbox', 'profile'];
+  // const tabComponents = [Home, ShoppingCartOrderScreen, AddContent, Home, Home];
+
   return (
     <Tab.Navigator
       labeled={false}
-      barStyle={{ height: 80 }}
+      barStyle={{ height: 80, display: showTabs ? 'flex' : 'none' }}
       // https://callstack.github.io/react-native-paper/
       // https://stackoverflow.com/questions/75013007/how-to-remove-this-white-ovale-behind-the-focused-in-the-material-bottom-tabs-na
       // edit theme later
+
+      screenOptions={{
+        tabBarIcon: (e) => <MaterialCommunityIcons name={'home'} size={26} />, // replace later
+      }}
     >
-      {createTabs()}
+      <Tab.Screen name="home" component={Home} />
+      <Tab.Screen name="cart" component={ShoppingCartOrderScreen} />
+      <Tab.Screen
+        name="create"
+        component={AddContent}
+        initialParams={{ setShowTabs }}
+      />
+      {/* replace ^ with useContext approach */}
+      <Tab.Screen name="inbox" component={Home} />
+      <Tab.Screen name="profile" component={Home} />
     </Tab.Navigator>
   );
 };
