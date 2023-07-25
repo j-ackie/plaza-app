5c4f5332ae616eb7bb36f5c1dde4077487ed2983
kyeoul <54390304+kyeoul@users.noreply.github.com>
2023-07-24 21:43:15 -0500

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -3,56 +3,16 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { PortalProvider } from '@gorhom/portal';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-<<<<<<< HEAD
-import { StatusBar } from "expo-status-bar";
-import Home from "./components/Home/Home";
-import Purchase from "./screens/Purchase";
-import ShoppingCartOrderScreen from "./components/ShoppingCartOrderScreen/ShoppingCartOrderScreen";
-import Modal from "./components/Modal/Modal";
-import { Audio } from "expo-av";
-import { StripeProvider } from "@stripe/stripe-react-native";
-import Confirm from "./screens/Confirm";
-import Profile from "./components/Profile/Profile";
-
-=======
 import { StatusBar } from 'expo-status-bar';
 import Home from './views/Home/Home';
 import ShoppingCartOrderScreen from './views/ShoppingCartOrderScreen/ShoppingCartOrderScreen';
 import { Audio } from 'expo-av';
 import AddContent from './views/AddContent/AddContent';
->>>>>>> 4910b399e923d807c0281b1105ba1915cdabcb02
 
 // https://reactnavigation.org/docs/material-bottom-tab-navigator
 const Tab = createMaterialBottomTabNavigator();
 const Stack = createStackNavigator();
 
-<<<<<<< HEAD
-const createTabs = (props) => {
-  console.log(props);
-  const tabNames = ["home", "cart", "create", "inbox", "profile"];
-  const tabComponents = [Home, ShoppingCartOrderScreen, Home, Home, Profile];
-
-  const tabs = [];
-  for (let i = 0; i < tabNames.length; i++) {
-    tabs.push(
-      <Tab.Screen
-        key={tabNames[i]}
-        name={tabNames[i]}
-        component={tabComponents[i]}
-        options={{
-          tabBarIcon: () => (
-            <MaterialCommunityIcons name={tabNames[0]} size={26} />
-          ), // replace later
-        }}
-      />
-    );
-  }
-
-  return tabs;
-};
-
-=======
->>>>>>> 4910b399e923d807c0281b1105ba1915cdabcb02
 const TabNavigator = () => {
   const [showTabs, setShowTabs] = useState(true);
 
@@ -80,7 +40,7 @@ const TabNavigator = () => {
       />
       {/* replace ^ with useContext approach */}
       <Tab.Screen name="inbox" component={Home} />
-      <Tab.Screen name="profile" component={Home} />
+      <Tab.Screen name="profile" component={} />
     </Tab.Navigator>
   );
 };
@@ -93,19 +53,18 @@ const App = () => {
   }, []);
 
   return (
-      <PortalProvider>
-        <StatusBar style="light"/>
-        <Stack.Navigator 
-          screenOptions={{
-            headerShown: false,
-            // container
-          }}
-        >
-          <Stack.Screen name="tabs" component={TabNavigator}/>
-          <Stack.Screen name="purchase" component={Purchase} options={{headerShown: true}} />
-          <Stack.Screen name="confirm" component={Confirm} options={{headerShown: true}} />
-        </Stack.Navigator>
-      </PortalProvider>
+    <PortalProvider>
+      <StatusBar style="light" />
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          // container
+        }}
+      >
+        <Stack.Screen name="tabs" component={TabNavigator} />
+        {/* <Stack.Screen name="purchase" component={Purchase} options={{headerShown: true}} /> */}
+      </Stack.Navigator>
+    </PortalProvider>
   );
 };
 
