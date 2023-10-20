efee125ba8b1dfcf5355a88bf9250eab6a576e54
Jackie Lin <jackielin2025@u.northwestern.edu>
2023-10-20 17:28:56 -0500

    useless.

--- app/api/utils.ts
+++
@@ -1,9 +0,0 @@
-const API_URL = 'http://localhost:8000/api';
-
-const fetchData = (endpoint: string, method: string) => {
-  return fetch(`${API_URL}${endpoint}`, {
-    method: method,
-  });
-};
-
-export default fetchData;
